9a75a3adf39abb885249f532093924b812f8ee5a
taufiqscript <28.taufiqurrahman@gmail.com>
2026-05-31 16:53:22 +0700

    add final link instagram

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -65,7 +65,7 @@ const Landing = () => {
                     <div className="hidden md:flex items-center justify-between max-w-7xl mx-auto px-6 md:px-10 py-3">
                         <div className="flex items-center gap-3">
                             <a
-                                href="https://www.instagram.com/riyadhussolihin.cb/"
+                                href="https://www.instagram.com/riyadhussholihin.cb/"
                                 target="=_blank"
                                 rel="noreferrer"
                                 className="bg-green-500 hover:bg-[#C13584] p-2 rounded-full">
@@ -671,7 +671,7 @@ const Landing = () => {
 
                             <div className="flex gap-4">
                                 <a
-                                    href="https://www.instagram.com/riyadhussolihin.cb/"
+                                    href="https://www.instagram.com/riyadhussholihin.cb/"
                                     className="w-11 h-11 rounded-full bg-white/10 flex items-center justify-center hover:bg-[#C13584] transition-all"
                                 >
                                     <FaInstagram className="text-white text-lg" />
